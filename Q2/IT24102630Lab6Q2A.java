151b00c35fff9622e823bdf00795014affdaaec2
IT24102630 <malminimasha73@gmail.com>
2024-09-16 13:32:35 +0530

    Add files via upload

---
+++ Q2/IT24102630Lab6Q2A.java
@@ -0,0 +1,17 @@
+public class IT24102630Lab6Q2A
+{
+	public static void main (String[]args)
+  {
+  
+   int count=1;
+  
+  while (count < 11)
+  {
+	  System.out.print(count +" ");
+	  
+		count++;
+  }
+  
+  }
+  
+}
