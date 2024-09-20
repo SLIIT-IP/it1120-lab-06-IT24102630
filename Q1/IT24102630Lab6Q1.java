f2202063dfe29a4aeebff4f97bf2efb2524c6028
IT24102630 <malminimasha73@gmail.com>
2024-09-20 21:13:18 +0530

    Update IT24102630Lab6Q1.java

--- Q1/IT24102630Lab6Q1.java
+++ Q1/IT24102630Lab6Q1.java
@@ -1,29 +1,21 @@
 import java.util.Scanner;
-public class IT24102630Lab6Q1
-{
-	public static void main (String[]args)
-  {
-	double number;
-	double square;
-	double root;
-	
-	Scanner input= new Scanner (System.in);
-	
-	
-	System.out.print("Enter a number: ");
-	number = input.nextDouble();
-	
+public class IT24102593Lab6Q1 {
 
-	square = number * number;
-	root = Math.sqrt(number);
-	
-	
-	System.out.println();
-	System.out.println("The square of "+ number +" is : "+ square);
-	System.out.print("The square root of "+number+" is : "+root);
-	
-  }
-  
+    public static void main(String[] args) {
+
+        Scanner value = new Scanner(System.in);
+
+        System.out.print("Enter a number:");
+        int number = value.nextInt();
+
+        double squareOfNumber = number * number;
+        System.out.println("The square of" + number+" is : " + squareOfNumber);
+
+        double squareRootOfNumber = Math.sqrt(number);
+        System.out.println("The square root of " + number + " is : " + squareRootOfNumber);
+
+    }
+
 }
 	
 	
